0cdd24e32d5b7f506bbbda3e3b8ac0276386f64f
Filip Zalewski <filip.zalewski@zs1mm.local>
2026-09-10 15:02:35 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
-
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router';
+import Cards from './components/Cards';
+import Table from './components/Table';
 
 function App() {
   useEffect(() => {
@@ -14,10 +17,44 @@ function App() {
   }, []);
 
   return (
-    <div className="App-header">
+    // display navbar at the top of the page, then empty page
+
+    <div className="App">
+      <BrowserRouter>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                {/* <li class="nav-item">
+                  <Link class="nav-link" to="/">Home</Link>
+                </li> */}
+                <li class="nav-item">
+                  <Link class="nav-link" to="/table">Table</Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" to="/cards">Cards</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/table" element={<Table/>} />
+          <Route path="/cards" element={<Cards />} />
+        </Routes>
+      </BrowserRouter >
+
       
+
     </div>
   );
 }
 
 export default App;
+
+
+// navbar: table, cards
---
+++ src/components/Table.js
@@ -0,0 +1,6 @@
+export default function Table() {
+  return (
+    <div className="container">
+    </div>
+  );
+}
